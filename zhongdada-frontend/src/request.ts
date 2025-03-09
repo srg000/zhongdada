3402fe771668792fd7ac9be5a1c73800ee8f4933
songrengang <songrengang@outlook.com>
2025-03-09 18:10:30 +0800

    update 更新后端地址

--- zhongdada-frontend/src/request.ts
+++ zhongdada-frontend/src/request.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
 import { Message } from "@arco-design/web-vue";
 
+// 是否是开发环境
+export const isDev = process.env.NODE_ENV === "development";
+
+// 创建 Axios 实例
 const myAxios = axios.create({
-  baseURL: "http://localhost:8101",
-  timeout: 100000,
+  baseURL: isDev
+    ? "http://localhost:8101"
+    : "https://zhongdada-backend-1222754-1305448123.ap-shanghai.run.tcloudbase.com/",
+  timeout: 60000,
   withCredentials: true,
 });
 
